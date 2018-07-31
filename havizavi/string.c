3fed360dded8dfc35d7408ecb8a8700d3fca5c90
Buddha Chandra Banik <buddhobaniks@gmail.com>
2018-07-31 18:24:15 +0600

    Update string.c

--- havizavi/string.c
+++ havizavi/string.c
@@ -6,7 +6,9 @@ char* f(char ch[])
 
     int i;
 
+    //if u use cpp
 //    char *c=new char( strlen(ch)+5);
+    //if u use c
     char *c=(char*)malloc((strlen(ch)+5)*sizeof(char));
     for(i=0; i<strlen(ch); i++)
         c[i] = ch[i];
